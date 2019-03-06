8d22ffcdb34c1f39af63df8465fc5339755c5938
UAB\wwarr <wwarr@uab.edu>
2019-03-06 10:39:35 -0600

    improved clarity, fixed issue with weighting

--- matlab_quantiles_example.m
+++ matlab_quantiles_example.m
@@ -1,6 +1,7 @@
 %% unsorted
-values = 5 * rand( 100, 1 );
-weights = rand( 100, 1 ) .^ 2;
+rng( 1618 )
+values = [ 0; 5 * rand( 100, 1 ) ];
+weights = [ rand( 100, 1 ) .^ 2; 0 ];
 
 %% quantiles to divisions
 QUANTILES = [ 0.05 0.25 0.5 0.75 0.95 ];
--- qd_unique.m
+++ qd_unique.m
@@ -1,4 +1,6 @@
 function [ values, weights ] = qd_unique( values, weights )
+% weights are between values with the final weight having no meaning
+% so the gap between v( i ) and v( i + 1 ) uses w( i )
 
 assert( isvector( values ) );
 assert( isnumeric( values ) );
@@ -10,14 +12,13 @@ samples = sortrows( [ values( : ) weights( : ) ], 1 );
 
 values = samples( :, 1 );
 [ unique_values, unique_indices ] = unique( values );
-unique_indices = length( values ) - flip( unique_indices ) + 1;
 
 weights = samples( :, 2 );
+weights = [ 0; mean( [ weights( 1 : end - 1 ) weights( 2 : end ) ], 2 ) ];
 normalized_cumulative_weight = cumsum( weights ) ./ sum( weights );
 unique_weights = normalized_cumulative_weight( unique_indices );
 
 [ weights, unique_indices ] = unique( unique_weights );
-weights = [ 0; weights( 2 : end ) ];
 values = unique_values( unique_indices );
 
 assert( ~isempty( values ) );
